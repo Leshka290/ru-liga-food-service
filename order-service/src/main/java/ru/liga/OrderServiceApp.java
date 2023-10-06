999f5a5f8f6199a364ba5ee50424643faf940d7e
leshka <lleesshhkkaa00@gmail.com>
2023-10-06 11:23:22 +0300

    add module OrderService refactoring dependency_bom (pom.xml)

--- order-service/src/main/java/ru/liga/OrderServiceApp.java
+++ order-service/src/main/java/ru/liga/OrderServiceApp.java
@@ -1,13 +1,14 @@
 package ru.liga;
 
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App{
-
+@Data
+public class OrderServiceApp {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(OrderServiceApp.class, args);
     }
 
 }
